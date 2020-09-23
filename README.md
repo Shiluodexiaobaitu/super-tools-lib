44a605dc9ca230e2e870086bf1c6a2edce5c9e9b
zc <zhangce_170@163.com>
2020-09-23 10:18:44 +0800

    添加h5获取经纬度方法

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-## 常用js工具函数
+# 常用js工具函数
+
+### map方法
+- lonLatToMercator,       // 经纬度转墨卡托
+- mercatorToLonlat,       // 墨卡托转经纬度
+- getPosition,            // H5 获取地理位置
--- utils/map.js
+++ utils/map.js
@@ -18,8 +18,45 @@
         return lonlat;
     }
 
+    /**
+     * H5 获取地理位置
+    */
+    const getPosition = (
+        timeout = 10000,
+        maximumAge = 60000,
+        enableHighAcuracy = false) => new Promise((resolve, reject) => {
+            if (!navigator && !navigator.geolocation) {
+                return reject(new Error('geolocation api not supported'))
+            }
+
+            const success = (loc) => {
+                const location = {
+                    latitude: loc.coords.latitude,  // 纬度
+                    longitude: loc.coords.longitude,  // 经度
+                    accuracy: loc.coords.accuracy // 精确度
+                }
+                resolve(location)
+            }
+
+            const error = () => reject('出错了')
+
+            navigator.geolocation.getCurrentPosition(success, error, {
+                enableHighAcuracy,  // 指示浏览器获取高精度的位置，默认为false
+                timeout,  // 指定获取地理位置的超时时间，默认不限时，单位为毫秒
+                maximumAge // 最长有效期，在重复获取地理位置时，此参数指定多久再次获取位置。
+            })
+        })
+
+    // 使用示例
+    // getPosition()
+    //     .then(pos => {
+    //         console.log('pos', pos)
+    //     })
+    //     .catch(err => console.log(err))
+
     return {
         lonLatToMercator,       // 经纬度转墨卡托
-        mercatorToLonlat        // 墨卡托转经纬度
+        mercatorToLonlat,       // 墨卡托转经纬度
+        getPosition,            // H5 获取地理位置
     }
 })()

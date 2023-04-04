c83d73cd6471a382da7cd1bf6d72b710a967efde
张策 <zhangce@fengmap.com>
2023-04-04 14:16:06 +0800

    补充注释

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,83 +1,57 @@
 
+// 生成一个唯一的guid
 export { guid } from './_guid'
 
+// 节流
 export { throttle } from './_throttle'
 
+// 防抖
 export { debounce } from './_debounce'
 
+// 模糊查询
 export { fuzzyQuery } from './_fuzzyQuery'
 
+// RGB颜色转16进制
 export { colorHex } from './_colorHex' 
 
+// 16进制颜色转RGBA
 export { hexToRgba } from './_hexToRgba'
 
+// rgba颜色转16进制
 export { rgbaToHex } from './_rgbaToHex'
 
+// 计算vh / vw转px
 export { viewportToPixels } from './_viewportToPixels'
 
+// 根据身份证号获取年龄
 export { getAge } from './_getAge'
 
+// 根据身份证号获取性别
 export { getSex } from './_getSex'
 
+// 数字转化为大写金额
 export { digitUppercase } from './_digitUppercase'
 
+// 动态引入js
 export { injectScript } from './_injectScript'
 
+// 汉字转字母
 export { sinogToLetter } from './_sinogToLetter' 
 
+// 返回设计稿上px在不同屏幕下的适配尺寸
 export { getFitSize } from './_getFitSize'
 
+// 检测密码强度
 export { checkPassWord } from './_checkPassWord'
 
+// 将华氏温度转换为摄氏温度
 export { fahrenheitToCelsius } from './_fahrenheitToCelsius'
 
+// 将摄氏温度转华氏温度
 export { celsiusToFahrenheit } from './_celsiusToFahrenheit'
 
+// 生成sign 按 Key 的 ASCII 正序排序，拼接为字符串返回
 export { generateSign } from './_generateSign'
 
+// 按 ASCII 正序排序
 export { sortAscii } from './_sortAscii'
-
-/**
- * @desc dom转img
- * 
- * @since 1.62.0
- * @param {*} id domID
- * @param {*} name 图片名
- * @param {*} cb 成功回调
- * @return {*}
- */
-// 依赖库版本： "html2canvas": "^1.4.1",
-//  export const domToImg = (id, name, cb) => {
-// 	window.pageYoffset = 0
-// 	document.documentElement.scrollTop = 0
-// 	document.body.scrollTop = 0
-// 	const node = document.getElementById(id)
-// 	const width = node.offsetWidth //dom宽
-// 	const height = node.offsetHeight //dom高
-// 	const scale = 2 //放大倍数 这个相当于清晰度 调大一点更清晰一点
-// 	html2canvas(node, {
-// 		width: width,
-// 		heigth: height,
-// 		backgroundColor: '#F5F7FB', //背景颜色 为null是透明
-// 		dpi: window.devicePixelRatio * 2, //按屏幕像素比增加像素
-// 		scale: scale,
-// 		X: 0,
-// 		Y: 0,
-// 		scrollX: -3, //如果左边多个白边 设置该偏移-3 或者更多
-// 		scrollY: -10,
-// 		useCORS: true, //是否使用CORS从服务器加载图像 !!!
-// 		allowTaint: true, //是否允许跨域图像污染画布  !!!
-// 	}).then((canvas) => {
-// 		const url = canvas.toDataURL() //这里上面不设值cors会报错
-// 		const a = document.createElement('a') //创建一个a标签 用来下载
-// 		a.download = name || '图片.png'
-// 		const event = new MouseEvent('click') //增加一个点击事件
-// 		//如果需要下载的话就加上这两句
-// 		a.href = url //此处的url为base64格式的图片资源
-// 		a.dispatchEvent(event) //触发a的单击事件 即可完成下载
-
-// 		if (cb && typeof cb === 'function') {
-// 			cb()
-// 		}
-// 	})
-// }
